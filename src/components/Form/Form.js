7b314d5676c72ffbb33a6fc1bd02c1ae36d94025
chrisIyer <hellohotbox420@gmail.com>
2023-02-05 21:23:18 -0500

    not rendering

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,64 +1,62 @@
 
-import  './Form.module.scss'
+import styles from './Form.module.scss'
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css'
-function FormIntake() {
+function FormIntake({
+  createBlog,
+  blog,
+  handleChange
+}) {
   return (
-     <div className='div'>
-    <Form>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+    <div className={styles.div}>
+      <Form className={styles.form}
+          onSubmit={(e) => {
+            e.preventDefault()
+            createBlog()
+          }}>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Blog Title</Form.Label>
+            <Form.Control className={styles.title}
+            type='text' 
+            value={blog.title} 
+            name='title' 
+            onChange={handleChange} 
+            placeholder='Title'  />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Date</Form.Label>
+            <Form.Control type="date" placeholder="Date" value={blog.date} 
+             />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label className={styles.one}>Mood</Form.Label>
+            <Form.Select size="lg" defaultValue="Pick me a color">
+              <option>Red...</option>
+              <option>White</option>
+              <option>Blue</option>
+            </Form.Select>
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="formGridAddress1">
+          <Form.Label>Blog Entry</Form.Label>
+          <Form.Control  size="lg"  placeholder="1234 Main St" />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
-      </Form.Group>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
+        <Form.Group className="mb-3" id="formGridCheckbox">
+          <Form.Check className={styles.checkbox} type="checkbox" label="Check me out" />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button className={styles.button} variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }

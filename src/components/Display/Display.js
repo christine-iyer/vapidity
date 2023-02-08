fc094c0df24c2e206b1e02501e66c7778e728dda
chrisIyer <hellohotbox420@gmail.com>
2023-02-08 17:57:06 -0500

    blog from todo.

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,25 +1,74 @@
 import Card from 'react-bootstrap/Card'
-import { useRef, useState, useEffect } from 'react'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Form from '../Form/Form'
 
-export default function Display ({...blogEntry}) {
-  const [blogDisplay, setBlogDisplay] = useState(false)
 
-  return (
-    <Card className='SubCard'>
-      <Card.Header className='SubCardHeader'>About Community</Card.Header>
-      <Card.Body className='SubCardContainer'>
-        <Card.Text className='AboutBlurb'>
-          {blogDisplay.title}
-        </Card.Text>
-        <br />
-        <Card.Text className='SubCardDate'>
+export default function BlogList({
+    entries,
+    addEntry,
+    completeEntry,
+    deleteEntry,
+    editEntryText
+  }) {
+    return (
+      <>
+      <div className='newentry'>
+        <h1>New Blog Entry </h1>
+        <input
+          type="text"
+          onKeyDown={(e) => {
+            e.key === "Enter" && addEntry(e)
+          }}
+        />
+        </div>
+        {entries.length ? (
+          <>
+<div className="nowlist">
 
-          <br />
+            <h1>My Entries</h1>
+            <ul>
+              {entries
+                .filter((i) => !i.completed)
+                .map((entry) => {
+                  return (
+                    <Form
+                      key={entry.id}
+                      entry={entry}
+                      completeEntry={completeEntry}
+                      deleteEntry={deleteEntry}
+                      editEntryText={editEntryText}
+                    />
+                  )
+                })}
+            </ul>
 
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  )
-}
+            </div>
+
+            <div className="neverlist">
+            
+            <h1>Archived Entries </h1>
+            <ul>
+              {entries
+                .filter((i) => i.completed)
+                .map((entry) => {
+                  return (
+                    <Form
+                      key={entry.id}
+                      entry={entry}
+                      completeTodo={completeEntry}
+                      deleteEntry={deleteEntry}
+                      editEntryText={editEntryText}
+                    />
+                  )
+                })}
+            </ul>
+            </div>
+          </>
+        ) : (
+          <h1>No Entries Added Yet</h1>
+        )}
+      </>
+    )
+  }
